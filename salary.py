0db86f0d00f798bad10d153e814f0dc5196874f8
Moncrief02 <fraziermoncrief@gmail.com>
2025-08-03 20:43:28 -0400

    integer change

---
+++ salary.py
@@ -0,0 +1,9 @@
+pay=input("please input starting salary: ")
+Max_hours=input("please input  maximum number of hours: "  )
+hours=int(Max_hours)
+start_salary=float(pay)
+print(start_salary,hours)
+week=start_salary *hours
+bi_week=week*2
+print(week,bi_week)
+
